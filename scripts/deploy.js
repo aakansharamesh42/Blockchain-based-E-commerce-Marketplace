d7e235b52a7355681498d4c248669e43a84ac287
Aakansha Ramesh <aakansharamesh42@gmail.com>
2023-04-10 20:21:11 +0530

    Added UI

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
+ // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
@@ -12,7 +12,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Setup accounts
+  const [deployer] = await ethers.getSigners()
 
+  //Deploy Dappazon
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log('Deployed Dappazon Contract at: ${dappazon.address}\n')
+
+  // List items...
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+    
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
